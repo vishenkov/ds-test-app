d2dad090ac7d5d89598885b0a06534ec1b51ebf8
Kirill Vishenkov <vishkir1991@gmail.com>
2024-05-01 15:59:53 +0300

    feat(init)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Modal} from 'tp-design-system';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Modal.Confirm />
       </header>
     </div>
   );
